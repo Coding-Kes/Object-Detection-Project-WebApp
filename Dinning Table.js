2baaf23af95a8d1f581db8ce73e447f08062347a
Coding-Kes <119104775+Coding-Kes@users.noreply.github.com>
2023-09-15 18:42:04 +1000

    Add files via upload

---
+++ Dinning Table.js
@@ -0,0 +1,62 @@
+
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+     img = loadImage("Dinnning Table.jpeg");
+}
+
+function setup()
+{
+    canvas = createCanvas(670, 510);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 670, 510);
+    
+    if(status != "")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Detected Objects";
+            
+            fill("#FFD700");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +" "+ percent +"%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke("FFD700");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            strokeWeight(2);
+        }
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error();
+    }
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
+}
+
+
+
